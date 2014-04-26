a4fe14d7c4497814fb872f127a43d06902d5d2e4
nelson pollard <nelson.pollard@sv.cmu.edu>
2014-04-26 14:01:21 -0700

    Got SearchEngineCSV (searching PW.csv with lucene) working Note: must compile by hand (i.e. javac SearchEngineCSV.java, etc.)

---
+++ SearchEngine/SearchEngineCSV.java
@@ -0,0 +1,88 @@
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopScoreDocCollector;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.Version;
+
+import java.io.*;
+
+public class SearchEngineCSV {
+    public static void main(String[] args) throws IOException, ParseException {
+			// 0. Specify the analyzer for tokenizing text.
+			//    The same analyzer should be used for indexing and searching
+			StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_40);
+
+			// 1. create the index
+			Directory index = new RAMDirectory();
+
+			IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_40, analyzer);
+
+      IndexWriter w = new IndexWriter(index, config);
+      String workingDir = System.getProperty("user.dir");
+      String filename = workingDir + File.separatorChar + "python_src/PW.csv";
+      System.out.printf("FILENAME: %s\n", filename);
+      //System.out.println(workingDir + File.separatorChar + "pw.csv");
+      addDoc(w, filename);
+      w.close();
+
+			// 2. query
+			String querystr = args.length > 0 ? args[0] : "telephony";
+
+			// the "title" arg specifies the default field to use
+			// when no field is explicitly specified in the query.
+			Query q = new QueryParser(Version.LUCENE_40, "tags", analyzer).parse(querystr);
+
+			// 3. search
+			int hitsPerPage = 10;
+			IndexReader reader = DirectoryReader.open(index);
+			IndexSearcher searcher = new IndexSearcher(reader);
+			TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage, true);
+			searcher.search(q, collector);
+			ScoreDoc[] hits = collector.topDocs().scoreDocs;
+
+			// 4. display results
+			System.out.println("Found " + hits.length + " hits.");
+			for(int i=0;i<hits.length;++i) {
+					int docId = hits[i].doc;
+					Document d = searcher.doc(docId);
+					System.out.println((i + 1) + ". NAME: " + d.get("name") + "\tRATING: " + d.get("rating") + "\t SUMMARY: " + d.get("summary") + "\tTAGS: " + d.get("tags"));
+			}
+
+			// reader can only be closed when there
+			// is no need to access the documents any more.
+			reader.close();
+    }
+
+    private static void addDoc(IndexWriter w, String file) throws IOException {
+      BufferedReader br = new BufferedReader(new FileReader(file));  
+      String line = null;  
+      while ((line = br.readLine()) != null) {
+			    Document doc = new Document();
+			    // PARSING WILL GO HERE
+          String[] fields = line.split(",");
+          /*for (int i = 0; i < fields.length; i++) {
+              System.out.printf("%d: %s\n", i, fields[i]);
+          }*/
+          if (fields.length == 4) {
+              doc.add(new TextField("name", fields[0], Field.Store.YES));
+              doc.add(new TextField("rating", fields[1], Field.Store.YES));
+              doc.add(new TextField("summary", fields[2], Field.Store.YES));
+              doc.add(new TextField("tags", fields[3], Field.Store.YES));
+			        w.addDocument(doc);
+          }
+      }
+    }
+}
