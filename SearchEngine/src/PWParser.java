6bb87422763839eec07b77e54b8d3da2ee28d9f0
Chris Lee <chris@the-eye.(none)>
2014-04-26 12:50:18 -0700

    Add content as comments. Only works some of the time

--- SearchEngine/src/PWParser.java
+++ SearchEngine/src/PWParser.java
@@ -1,7 +1,10 @@
+package SearchEngine;
+
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.lang.Thread;
 
 //It begins
 import org.apache.axiom.om.OMElement;
@@ -20,16 +23,19 @@ import javax.xml.namespace.QName;
 import java.io.ByteArrayInputStream;
 import java.util.Iterator;
 
+import SearchEngine.SearchEngine;
+
 public class PWParser {
 
     public static void main(String[] args) throws Exception {
-        System.setProperty("javax.net.ssl.trustStore", "src/wso2carbon.jks");
-        System.setProperty("javax.net.ssl.trustStorePassword", "wso2carbon");
-        System.setProperty("javax.net.ssl.trustStoreType","JKS");
-        String REGISTRY_URL = "https://localhost:9443/registry/atom";
-        String USER = "admin";
-        String PASSWORD = "admin";
-        RemoteRegistry registry = new RemoteRegistry(REGISTRY_URL, USER, PASSWORD);
+//        System.setProperty("javax.net.ssl.trustStore", "src/wso2carbon.jks");
+//        System.setProperty("javax.net.ssl.trustStorePassword", "wso2carbon");
+//        System.setProperty("javax.net.ssl.trustStoreType","JKS");
+//        String REGISTRY_URL = "https://localhost:9443/registry/atom";
+//        String USER = "admin";
+//        String PASSWORD = "admin";
+//        RemoteRegistry registry = new RemoteRegistry(REGISTRY_URL, USER, PASSWORD);
+        RemoteRegistry registry = SearchEngine.getRegistry();
         Scanner stdin = new Scanner(System.in);
         String line = "";
         while(stdin.hasNextLine()) {
@@ -45,31 +51,18 @@ public class PWParser {
                 Resource r1 = registry.newResource();
                 r1.setContent(line.getBytes());
                 registry.put("/c1/pw" + tokens[0], r1);
+                System.out.println("/c1/pw" + tokens[0]);
+            }
+            try {
+                if (registry.getComments("/c1/pw" + tokens[0]).length < 2) {
+                    Comment c1 = new Comment();
+                    c1.setText(line);
+                    registry.addComment("/c1/pw" + tokens[0], c1);
+                }
+            }
+            catch (Exception e) {
+                System.out.println("Comment insert failed" + e);
             }
         }
-//        if (!registry.resourceExists("/c1/pw")) {
-//            System.out.println("Resource Exists");
-//           Collection collection = registry.newCollection();
-//           registry.put("/c1/pw", collection);
-//        }
-//        
-//        if (!registry.resourceExists("/c1/pw/r1")) {
-//            System.out.println("Set r1");
-//            Resource r1 = registry.newResource();
-//            String r1_str = "My File Content";
-//            r1.setContent(r1_str.getBytes());
-//            registry.put("/c1/pw/r1", r1);
-//        }
-
-        
-//        try {
-//            if (registry.getComments("/c1/c2/r1")[0] != null) {
-//                Comment c1 = new Comment();
-//                c1.setText("This is my comment");
-//                registry.addComment("/c1/c2/r1", c1);
-//            }
-//        }
-//        catch (Exception e) {
-//        }
     }
 }
