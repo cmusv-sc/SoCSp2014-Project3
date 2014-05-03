e64efde01e00ba58dd7aed490422054e97657bb0
nelson pollard <nelson.pollard@sv.cmu.edu>
2014-05-03 12:15:24 -0700

    Major refactor of SearchEngineCSV

--- SearchEngine/SearchEngineCSV.java
+++ SearchEngine/SearchEngineCSV.java
@@ -20,62 +20,37 @@ import org.apache.lucene.util.Version;
 import java.io.*;
 
 public class SearchEngineCSV {
-    public static void main(String[] args) throws IOException, ParseException {
-			// 0. Specify the analyzer for tokenizing text.
-			//    The same analyzer should be used for indexing and searching
-			StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_40);
+     StandardAnalyzer analyzer;
+     Directory index;
+     IndexWriterConfig config;
+     IndexWriter w;
+     String filename;
+     IndexReader reader;
+		 IndexSearcher searcher;
+      
 
-			// 1. create the index
-			Directory index = new RAMDirectory();
+     public SearchEngineCSV (String filename, String field, String value) throws IOException, ParseException {
+      
+       // Add document
+       analyzer = new StandardAnalyzer(Version.LUCENE_40);
+       index = new RAMDirectory();
+       config = new IndexWriterConfig(Version.LUCENE_40, analyzer);
+       w = new IndexWriter(index, config);
+       this.filename = filename;
+       System.out.printf("FILENAME: %s\n", filename);
+       addDoc(w, filename);
+       w.close();
 
-			IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_40, analyzer);
+       // Search document
+       Query q = buildQuery(field, value);
+       reader = DirectoryReader.open(index);
+			 searcher = new IndexSearcher(reader);
+       ScoreDoc[] hits = getHits(q);
+       printResults(hits);
+       reader.close();
 
-      IndexWriter w = new IndexWriter(index, config);
-      String workingDir = System.getProperty("user.dir");
-      String filename = workingDir + File.separatorChar + "python_src/PW.csv";
-      System.out.printf("FILENAME: %s\n", filename);
-      //System.out.println(workingDir + File.separatorChar + "pw.csv");
-      addDoc(w, filename);
-      w.close();
-
-			// 2. query
-      String field = "tags";
-      String querystr = "tag1";
-      if (args.length == 1) {
-        querystr = args[0];
-      } else if (args.length == 2) {
-        field = args[0];
-        querystr = args[1];
-      }
-      System.out.println("QUERY: " + field + " = " + querystr);
-			Query q = new QueryParser(Version.LUCENE_40, field, analyzer).parse(querystr);
-
-			// 3. search
-			int hitsPerPage = 10;
-			IndexReader reader = DirectoryReader.open(index);
-			IndexSearcher searcher = new IndexSearcher(reader);
-			TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage, true);
-			searcher.search(q, collector);
-			ScoreDoc[] hits = collector.topDocs().scoreDocs;
-
-			// 4. display results
-			System.out.println("Found " + hits.length + " hits.");
-			for(int i=0;i<hits.length;++i) {
-					int docId = hits[i].doc;
-					Document d = searcher.doc(docId);
-          System.out.println();
-					System.out.println((i + 1) + ". NAME: " + d.get("name")); 
-          System.out.println("\tDESCRIPTION: " + d.get("description"));
-          System.out.println("\tRATING: " + d.get("rating"));
-          System.out.println("\tTAGS: " + d.get("tags"));
-          System.out.println("\tTIMESTAMP: " + d.get("timestamp"));
-			}
-
-			// reader can only be closed when there
-			// is no need to access the documents any more.
-			reader.close();
     }
-
+ 
     private static void addDoc(IndexWriter w, String file) throws IOException {
       BufferedReader br = new BufferedReader(new FileReader(file));  
       String line = null;  
@@ -90,4 +65,50 @@ public class SearchEngineCSV {
 			    w.addDocument(doc);
       }
     }
+  
+    private Query buildQuery (String field, String value) throws ParseException {
+      System.out.println("QUERY: " + field + " = " + value);
+			return new QueryParser(Version.LUCENE_40, field, analyzer).parse(value);
+    
+    }
+
+    private ScoreDoc[] getHits(Query q) throws IOException {
+			int hitsPerPage = 10;
+			TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage, true);
+			searcher.search(q, collector);
+			return collector.topDocs().scoreDocs;
+    
+    }
+
+    private void printResults(ScoreDoc[] hits) throws IOException {
+      System.out.println("Found " + hits.length + " hits.");
+			for(int i=0;i<hits.length;++i) {
+					int docId = hits[i].doc;
+					Document d = searcher.doc(docId);
+          System.out.println();
+					System.out.println((i + 1) + ". NAME: " + d.get("name")); 
+          System.out.println("\tDESCRIPTION: " + d.get("description"));
+          System.out.println("\tRATING: " + d.get("rating"));
+          System.out.println("\tTAGS: " + d.get("tags"));
+          System.out.println("\tTIMESTAMP: " + d.get("timestamp"));
+			}
+    
+    }
+
+    public static void main(String[] args) throws IOException, ParseException {
+      String workingDir = System.getProperty("user.dir");
+      String filename = workingDir + File.separatorChar + "python_src/PW.csv";
+String field = "tags";
+      String querystr = "tag1";
+      if (args.length == 1) {
+        querystr = args[0];
+      } else if (args.length == 2) {
+        field = args[0];
+        querystr = args[1];
+      }
+
+      SearchEngineCSV searchifier = new SearchEngineCSV(filename, args[0], args[1]);
+
+    }
+
 }
