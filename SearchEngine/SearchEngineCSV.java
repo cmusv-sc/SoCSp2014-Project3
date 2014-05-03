d737d71823417ca1e31e428a6bdfc0567e918caf
nelson pollard <nelson.pollard@sv.cmu.edu>
2014-05-03 11:36:56 -0700

    Prettied up output.

--- SearchEngine/SearchEngineCSV.java
+++ SearchEngine/SearchEngineCSV.java
@@ -64,8 +64,11 @@ public class SearchEngineCSV {
 					int docId = hits[i].doc;
 					Document d = searcher.doc(docId);
           System.out.println();
-					System.out.println((i + 1) + ". NAME: " + d.get("name") + "\tDESCRIPTION" + d.get("description") + "\tRATING: " + d.get("rating") + "\tTAGS: " + d.get("tags") + "\tTIMESTAMP: " + d.get("timestamp"));
-          System.out.println();
+					System.out.println((i + 1) + ". NAME: " + d.get("name")); 
+          System.out.println("\tDESCRIPTION: " + d.get("description"));
+          System.out.println("\tRATING: " + d.get("rating"));
+          System.out.println("\tTAGS: " + d.get("tags"));
+          System.out.println("\tTIMESTAMP: " + d.get("timestamp"));
 			}
 
 			// reader can only be closed when there
